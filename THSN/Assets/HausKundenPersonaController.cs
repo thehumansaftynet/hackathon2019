4f4d152afc660a3c1f2fe8953f9b7cfcd6aa7023
rings <rings@informatik.uni-hamburg.de>
2019-10-29 16:58:48 +0100

    Add Personas

--- THSN/Assets/HausKundenPersonaController.cs
+++ THSN/Assets/HausKundenPersonaController.cs
@@ -7,19 +7,54 @@ public class HausKundenPersonaController : MonoBehaviour
 
     public GameObject InterviewTextIntro;
     public GameObject PersonaTextIntro;
+    public GameObject Creator;
+
+    public GameObject BackgroundManu;
+
+    public int[] maxValues;
 
     public DragWithMomentum TextScollingScript;
 
 
+    private int state = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //PersonaTextIntro.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NextStep()
+    {
+        state++;
+
+        Transform canvas;
+
+        switch (state)
+        {
+            case 1:
+                PersonaTextIntro.SetActive(false);
+                InterviewTextIntro.SetActive(true);
+                canvas = InterviewTextIntro.transform.Find("Canvas");
+                TextScollingScript.ScrollPane = canvas.gameObject;
+                break;
+            case 2:
+                InterviewTextIntro.SetActive(false);
+                BackgroundManu.SetActive(false);
+                canvas = InterviewTextIntro.transform.Find("Canvas");
+                TextScollingScript.ScrollPane = canvas.gameObject;
+                Creator.SetActive(true);
+                break;
+            case 3:
+                Creator.SetActive(false);
+                break;
+        }
+
     }
 }
---
+++ THSN/Assets/PersonaCratorController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersonaCratorController : MonoBehaviour
+{
+
+    public Toggle[] Bildung;
+    public Toggle[] Geschlecht;
+    public Toggle[] Alter;
+    public Toggle[] Einkommen;
+    public Toggle[] Haushalt;
+    public Toggle[] Familienstand;
+
+    public Sprite[] PersonaBilder;
+    public Sprite Questionmark;
+    public Sprite Upload;
+    public Image Avatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CheckboxChanged()
+    {
+        string imagename = $"{getActiveID(Bildung)}_{getActiveID(Geschlecht)}_{getActiveID(Alter)}";
+
+        Debug.Log($"Change {imagename}");
+        bool found = false;
+        foreach (var s in PersonaBilder)
+        {
+            if (StringComparison(s.name, imagename))
+            {
+                found = true;
+                Avatar.GetComponent<Image>().sprite = s;
+            }
+        }
+
+        if (!found)
+        {
+            Debug.Log($"Not found {imagename}");
+            Avatar.GetComponent<Image>().sprite = Questionmark;
+        }
+    }
+
+    public static bool StringComparison(string s1, string s2)
+    {
+        if (s1.Length != s2.Length) return false;
+        for (int i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getActiveID(Toggle[] toggles)
+    {
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            if (toggles[i].isOn)
+            {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+
+}
