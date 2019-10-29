6c2339de9516e4b91c9e32a70cea6a3f74a89284
rings <rings@informatik.uni-hamburg.de>
2019-10-29 17:32:41 +0100

    Neue Personas und map

---
+++ THSN/Assets/AppController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppController : MonoBehaviour
+{
+
+    //public static AppController Instance;
+
+    private void Awake()
+    {
+        //if(Instance != null)
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+
+        //Instance = this;
+        //DontDestroyOnLoad(gameObject);
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartPersoneHouseScene()
+    {
+        SceneManager.LoadScene("HausKundenPersona");
+    }
+
+    public void StartMarketScene()
+    {
+        SceneManager.LoadScene("MarktplatzScene");
+    }
+
+    public void StartMapScene()
+    {
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
